0811be106f2f68fe987a8676e8922441d26a395a
Nemanja <nemanjaapopovic@gmail.com>
2023-12-29 07:20:24 +0100

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import Header from "../components/Header";
+import Header from "@/components/Header";
 import { Roboto } from "next/font/google";
 import styles from "./globals.css";
 import Footer from "@/components/Footer";
