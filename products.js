4fef7fd44719969d8aacae78cf5c6d7fc9344f03
Nemanja <nemanjaapopovic@gmail.com>
2023-12-29 06:37:25 +0100

    Update products.js

--- products.js
+++ products.js
@@ -3,16 +3,19 @@ const products = [
     id: 1,
     title: "Steznik",
     image_url: "/assets/images/steznik-za-koleno.png",
+    description: "Steznik za koleno",
   },
   {
     id: 2,
     title: "Ortoza za vrat",
     image_url: "/assets/images/ortoza-za-vrat.jpeg",
+    description: "Ortoza za vrat",
   },
   {
     id: 3,
     title: "Perika",
     image_url: "/assets/images/barbara.jpeg",
+    description: "Perika, plava kratka kosa",
   },
 ];
 
