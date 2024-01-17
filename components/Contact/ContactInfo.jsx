a91405f8b2fdf66445696e04d04fd15429a7d7ad
Nemanja <nemanjaapopovic@gmail.com>
2024-01-17 15:47:31 +0100

    update
    added alt to icons in contact info and update css in header.module.css

--- components/Contact/ContactInfo.jsx
+++ components/Contact/ContactInfo.jsx
@@ -8,25 +8,29 @@ const ContactInfo = () => {
     <div className={styles.contact__info}>
       <div className={styles.info__content}>
         <div className="icon__wrapper">
-          <Image src="/assets/icons/icon-time.svg" fill />
+          <Image src="/assets/icons/icon-time.svg" fill alt="working time" />
         </div>
         <span>8:30h - 19h, Subotom 9h - 14h</span>
       </div>
       <div className={styles.info__content}>
         <div className="icon__wrapper">
-          <Image src="/assets/icons/icon-marker.svg" fill />
+          <Image src="/assets/icons/icon-marker.svg" fill alt="adress" />
         </div>
         <span>Omladinska 50, Užice 31000</span>
       </div>
       <div className={styles.info__content}>
         <div className="icon__wrapper">
-          <Image src="/assets/icons/icon-phone.svg" fill />
+          <Image src="/assets/icons/icon-phone.svg" fill alt="phone number" />
         </div>
         <span>+381 31 513-889</span>
       </div>
       <div className={styles.info__content}>
         <div className="icon__wrapper">
-          <Image src="/assets/icons/icon-smartphone.svg" fill />
+          <Image
+            src="/assets/icons/icon-smartphone.svg"
+            fill
+            alt="mobile phone number"
+          />
         </div>
         <span>+381641721272</span>
       </div>
