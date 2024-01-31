643e408f360aaf6c4e9e34ec8f3c1aa073c19744
Nemanja <nemanjaapopovic@gmail.com>
2024-01-31 19:53:05 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,8 +1,8 @@
 const products = [
   {
     id: 1,
-    title: "Štitnik nosa",
-    image_url: "/assets/images/ortoze-pojasevi-mideri/štitnik-nosa.png",
+    title: "štitnik-nosa",
+    image_url: "/assets/images/ortoze-pojasevi-mideri/stitnik-nosa.png",
     description: "Štitnik nosa",
     category: "kolica",
   },
@@ -16,7 +16,7 @@ const products = [
   {
     id: 3,
     title: "Trudnički pojas",
-    image_url: "/assets/images/ortoze-pojasevi-mideri/trudnički-pojas.png",
+    image_url: "/assets/images/ortoze-pojasevi-mideri/trudnicki-pojas.png",
     description: "Trudnički pojas",
     category: "mideri",
   },
